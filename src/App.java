920820fc543b69fd54919d60d72b7ed30e4cc9cb
Janssen Batista <batistajanssen.dev@gmail.com>
2022-02-02 19:37:44 -0300

    Refactoring

--- src/App.java
+++ src/App.java
@@ -3,7 +3,6 @@ import java.util.Scanner;
 public class App {
 
     private static int workTimeCount;
-    private static int breakTime;
     private static int breakTimeCount;
     private static int longBreakTimeCount;
     private static int minutes;
@@ -33,6 +32,8 @@ public class App {
                     break;
                 case -1:
                     break;
+                default:
+                    System.err.println("Invalid option. Try again!");
             }
         }
 
@@ -45,44 +46,52 @@ public class App {
 
     public static void runWorkTime() throws InterruptedException {
         int workTime = 25 * 60 * 1000;
-        minutes = workTime / 60 / 1000 % 60;
+        minutes = timeInMinutes(workTime) % 60;
         System.out.println("====== work time =======");
         while (workTime > 0) {
             System.out.format("%02d:%02d\n", minutes, seconds);
             Thread.sleep(1000);
             workTime -= 1000;
-            minutes = (workTime / 60 / 1000) % 60;
-            seconds = (workTime / 1000) % 60;
+            minutes = timeInMinutes(workTime) % 60;
+            seconds = timeInSeconds(workTime) % 60;
         }
         workTimeCount++;
     }
 
     public static void runBreakTime() throws InterruptedException {
-        breakTime = 5 * 60 * 1000;
-        minutes = breakTime / 60 / 1000 % 60;
+        int breakTime = 5 * 60 * 1000;
+        minutes = timeInMinutes(breakTime) % 60;
         System.out.println("====== long break time =======");
         while (breakTime > 0) {
             System.out.format("%02d:%02d\n", minutes, seconds);
             Thread.sleep(1000);
             breakTime -= 1000;
-            minutes = (breakTime / 60 / 1000) % 60;
-            seconds = (breakTime / 1000) % 60;
+            minutes = timeInMinutes(breakTime) % 60;
+            seconds = timeInSeconds(breakTime) % 60;
         }
         breakTimeCount++;
     }
 
     public static void runLongBreakTime() throws InterruptedException {
         int longBreakTime = 14 * 60 * 1000;
-        minutes = longBreakTime / 60 / 1000 % 60;
+        minutes = timeInMinutes(longBreakTime) % 60;
         System.out.println("====== long break time =======");
         while (longBreakTime > 0) {
             System.out.format("%02d:%02d\n", minutes, seconds);
             Thread.sleep(1000);
             longBreakTime -= 1000;
-            minutes = longBreakTime / 60 / 1000 % 60;
-            seconds = longBreakTime / 1000 % 60;
+            minutes = timeInMinutes(longBreakTime) % 60;
+            seconds = timeInSeconds(longBreakTime) % 60;
         }
         longBreakTimeCount++;
+    }
+
+    private static int timeInMinutes(int time) {
+        return time / 60 / 1000;
+    }
+
+    private static int timeInSeconds(int time) {
+        return  time / 1000;
     }
 
 }
